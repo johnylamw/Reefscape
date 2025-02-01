e1407e4a6e7ff1868e7e5014789cf03753be9232
johnylamw <johnylamw@gmail.com>
2025-02-01 15:41:15 -0800

    Added PoseEstimator

--- reef_AT.py
+++ reef_AT.py
@@ -3,7 +3,11 @@ https://robotpy.readthedocs.io/projects/apriltag/en/latest/robotpy_apriltag.html
 """
 import cv2
 import numpy as np
-from robotpy_apriltag import AprilTagField, AprilTagFieldLayout, AprilTagDetector
+from robotpy_apriltag import \
+    AprilTagField, AprilTagFieldLayout, AprilTagDetector, \
+    AprilTagPoseEstimator
+from wpimath.geometry import Transform3d
+import json
 
 image_path = "./image.png"
 image = cv2.imread(image_path)
@@ -11,6 +15,18 @@ grayscale_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
 detector =  AprilTagDetector()
 detector.addFamily("tag36h11")
+
+with open("rearleft.json") as PV_config:
+    data = json.load(PV_config)
+    cameraIntrinsics = data["cameraIntrinsics"]["data"]
+    fx = cameraIntrinsics[0]
+    fy = cameraIntrinsics[4]
+    cx = cameraIntrinsics[2]
+    cy = cameraIntrinsics[5]
+
+# Tag Size: 165.1 mm = 0.1651 m
+config = AprilTagPoseEstimator.Config(tagSize=0.1651, fx=fx, fy=fy, cx=cx, cy=cy)
+estimator = AprilTagPoseEstimator(config)
 
 output = detector.detect(grayscale_image)
 for detections in output:
@@ -30,9 +46,12 @@ for detections in output:
     centerY = detections.getCenter().y
     cv2.circle(image, (int(centerX), int(centerY)), 2, color=(0, 255, 255), thickness=3)
 
-    # Get Homography Information:
-    print(detections.getHomographyMatrix())
-cv2.namedWindow("Polygon", cv2.WINDOW_GUI_NORMAL)
+    # Get Homography Information
+    estimation = AprilTagPoseEstimator.estimateHomography(estimator, detections)
+    print(estimation)
+    print("matrix", estimation.toMatrix())
+    print("rotation", estimation.rotation())
+    print("translation", estimation.translation())
 
 while True:
     cv2.imshow("frame", image)
