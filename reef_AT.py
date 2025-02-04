8bb671bb4b5b1a8663f2c53f68e9329f5709253c
johnylamw <johnylamw@gmail.com>
2025-02-04 14:22:48 -0800

    wip tag translation

--- reef_AT.py
+++ reef_AT.py
@@ -3,12 +3,13 @@ https://robotpy.readthedocs.io/projects/apriltag/en/latest/robotpy_apriltag.html
 https://pypi.org/project/photonlibpy/
 PORT TO PHOTONVISION LATER: https://docs.photonvision.org/en/latest/docs/programming/photonlib/index.html
 
-
 Steps:
 1. Get the pose of AT
-2. Find the 3D offsets of the AT -> reef branches
+2. Find the 3D offsets of the AT -> reef branches (have a cad model for that)
 3. Transform locations of reef branches via depth data -> camera frame 
 4. Detect if objects are "fixed" onto those branches
+
+Measurement in inches
 """
 import cv2
 import numpy as np
@@ -24,6 +25,10 @@ grayscale_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
 detector =  AprilTagDetector()
 detector.addFamily("tag36h11")
+
+reef_tag_frame = [
+    np.array([-6.468, 12.833, 8.986]) # L2:R
+]
 
 with open("rearleft.json") as PV_config:
     data = json.load(PV_config)
@@ -56,11 +61,10 @@ for detections in output:
     cv2.circle(image, (int(centerX), int(centerY)), 2, color=(0, 255, 255), thickness=3)
 
     # Get Homography Information
-    estimation = AprilTagPoseEstimator.estimateHomography(estimator, detections)
-    print(estimation)
     print("matrix", estimation.toMatrix())
-    print("rotation", estimation.rotation())
-    print("translation", estimation.translation())
+    estimation = AprilTagPoseEstimator.estimateHomography(estimator, detections)
+    rotation3d = estimation.rotation()
+    translation3d = estimation.translation()
 
 while True:
     cv2.imshow("frame", image)
