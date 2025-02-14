16dedb3a470d1ba249538f0ee1453429ecc15044
johnylamw <johnylamw@gmail.com>
2025-02-13 20:54:02 -0800

    helper functions

--- Reef.py
+++ Reef.py
@@ -6,65 +6,84 @@ class Alliance(Enum):
     RED = 0, "Red"
     BLUE = 1, "Blue"
 
+class Direction(Enum):
+    LEFT = 0
+    RIGHT = 1
 
 class Reef:
 
-    class State(Enum):
-        OFF = 0,
+    class BranchState(Enum):
+        OFF = 0
         ON = 1
     
     class Level(Enum):
-        L2 = 0, # Index 
-        L3 = 1,
+        L2 = 0
+        L3 = 1
         L4 = 2
 
-    # A, B, C, D, E, F, G, H, I, J, K, L
     global BLUE_ALLIANCE_TAGS
     global RED_ALLIANCE_TAGS 
+    global BRANCHES_COL
+    # TAG INDEXES [A, B, C, D, E, F, G, H, I, J, K, L]
     BLUE_ALLIANCE_TAGS = [18, 17, 22, 21, 20, 19]
     RED_ALLIANCE_TAGS = [7, 8, 9, 10, 11, 6]
+    BRANCHES_COL = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L"]
+    
     def __init__(self, alliance : Alliance):
         self.alliance = alliance
         assert self.alliance == Alliance.RED or self.alliance == Alliance.BLUE, "Alliance Not Properly Initialized"
 
-        self.branch_state = {
-            "A" : self.State.OFF,
-            "B" : self.State.OFF,
-            "C" : self.State.OFF,
-            "D" : self.State.OFF,
-            "E" : self.State.OFF,
-            "F" : self.State.OFF,
-            "G" : self.State.OFF,
-            "H" : self.State.OFF,
-            "I" : self.State.OFF,
-            "J" : self.State.OFF,
-            "K" : self.State.OFF,
-            "L" : self.State.OFF,
-        }
-
-        if self.alliance == Alliance.RED:
-            branch_to_tag = {
-                "A": RED_ALLIANCE_TAGS[0], "B": RED_ALLIANCE_TAGS[0],
-                "C": RED_ALLIANCE_TAGS[1], "D": RED_ALLIANCE_TAGS[1],
-                "E": RED_ALLIANCE_TAGS[2], "F": RED_ALLIANCE_TAGS[2],
-                "G": RED_ALLIANCE_TAGS[3], "H": RED_ALLIANCE_TAGS[3],
-                "I": RED_ALLIANCE_TAGS[4], "J": RED_ALLIANCE_TAGS[4],
-                "K": RED_ALLIANCE_TAGS[5], "L": RED_ALLIANCE_TAGS[5],
-            }
-        else: # Blue Alliance
-            branch_to_tag = {
-                "A": BLUE_ALLIANCE_TAGS[0], "B": BLUE_ALLIANCE_TAGS[0],
-                "C": BLUE_ALLIANCE_TAGS[1], "D": BLUE_ALLIANCE_TAGS[1],
-                "E": BLUE_ALLIANCE_TAGS[2], "F": BLUE_ALLIANCE_TAGS[2],
-                "G": BLUE_ALLIANCE_TAGS[3], "H": BLUE_ALLIANCE_TAGS[3],
-                "I": BLUE_ALLIANCE_TAGS[4], "J": BLUE_ALLIANCE_TAGS[4],
-                "K": BLUE_ALLIANCE_TAGS[5], "L": BLUE_ALLIANCE_TAGS[5],
-            }
-
-        branch_state = {
-            "A" : {self.Level.L2: self.State.OFF}
-        }
-
+        # Initialize
+        self.init_alliance_settings()
+        self.init_branch_to_tag()
+        self.init_branch_states()
         
+    def init_alliance_settings(self):
+        # Initialize Alliance Specific Settings
+        if self.alliance == Alliance.RED:
+            self.alliance_tags = RED_ALLIANCE_TAGS
+        elif self.alliance == Alliance.BLUE:
+            self.alliance_tags = BLUE_ALLIANCE_TAGS
 
-    def toggle_branch(self, )
+    def init_branch_to_tag(self):
+        # Branch Char -> Tag Dictionary 
+        self.branch_to_tag = {}
+        index = 0
+        char_index = 0
+        for col in BRANCHES_COL:
+            self.branch_to_tag.update({col : self.alliance_tags[index]})
+            char_index += 1
+            if char_index % 2 == 0:
+                index += 1
+    
+    def init_branch_states(self):
+        #Initialize Branch States:
+        self.branch_state = {}
+        for col in BRANCHES_COL:
+            # Initialize L2, L3, L4
+            self.branch_state.update({col : {
+                self.Level.L2: self.BranchState.OFF,
+                self.Level.L3 : self.BranchState.OFF,
+                self.Level.L4 : self.BranchState.OFF}})
+    
+    def get_all_states(self):
+        return self.branch_state
+
+    # Get the state of the branches
+    def get_branch_column(self, col : chr):
+        return self.branch_state.get(col)
+
+    def get_branch_state_at(self, col : chr, level : Level):
+        column = self.get_branch_column(col)
+        return column.get(level)
+    
+    def get_branches_at_tag(self, id : int):
+        pass
+    
+    def get_tag_from_col(self, col : chr):
+        pass
+    #def toggle_branch(self, )
+
+#test = Reef(Alliance.RED)
+#print(test.get_branch_state_at('A', Reef.Level.L2))
+#print(test.get_all_states())
